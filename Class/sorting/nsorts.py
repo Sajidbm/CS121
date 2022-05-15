f108220cee58728a0d9d1f2f53ba1ca8a5406671
Sajid <sajidmahmud61@gmail.com>
2022-05-14 19:49:58 -0700

    did not work last time, redoing

--- Class/sorting/nsorts.py
+++ Class/sorting/nsorts.py
@@ -1,5 +1,6 @@
 def bubbleSort(lista):
     n = len(lista)
     for element in range(0,n-1):
+        #removed the prints
         if lista[element] >= lista[element+1]:
             lista[element], lista[element+1] = lista[element+1], lista[element]
