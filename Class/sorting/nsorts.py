08641cbd293c248d50050d4f2e9fe4d025045245
Sajid <sajidmahmud61@gmail.com>
2022-05-15 10:41:51 -0700

    after long mess

--- Class/sorting/nsorts.py
+++ Class/sorting/nsorts.py
@@ -1,4 +1,11 @@
+#the comments help troubleshoot
 def bubbleSort(lista):
     n = len(lista)
-    for element in range(0,n-1):
-
+    for turn in range(n,1,-1):
+        #print("Outer:"+str(turn)+"\n")
+        #print("-")
+        for element in range(turn-1):
+            #the choice of range: if it's not turn-1 then we go out of the range
+            print(element)
+            if lista[element] >= lista[element+1]:
+                lista[element], lista[element+1] = lista[element+1], lista[element]
